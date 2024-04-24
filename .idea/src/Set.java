497d8712e4c5e42a8859eb39807610ac2f2a0538
Islombekk1 <saidvsislom@gmail.com>
2024-04-24 12:14:01 +0500

    Task4

---
+++ .idea/src/Set.java
@@ -0,0 +1,77 @@
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Set implements Collection {
+    String[] arr = new String[5];
+
+    public String set(){
+        for (int i = 0; i < arr.length; i++){
+
+        }
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
