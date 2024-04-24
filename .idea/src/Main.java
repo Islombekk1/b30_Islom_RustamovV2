73f0157799e8e1618817ba871546f6a3bdffcd84
Islombekk1 <saidvsislom@gmail.com>
2024-04-24 10:13:48 +0500

    sa

---
+++ .idea/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Salom");
+        System.out.println("Salom");
+        System.out.println("Salom");
+        System.out.println("Salom");
+    }
+
+}
